6f7a19f8d2c27b2242b9a77d285082687a9ded7f
David Chen <youdinghuan.chen.gr@dartmouth.edu>
2017-05-29 16:26:14 -0400

    more clinical trial descriptive stats visualization

--- Public-Repositories-Exploration/ClinTrialStats.R
+++ Public-Repositories-Exploration/ClinTrialStats.R
@@ -7,31 +7,37 @@ library(ggplot2)
 library(gridExtra)
 library(stringr)
 
-temodar <- read.csv("~/repos/Bench2Bioinfo/Public-Repositories-Exploration/ClinTrialStats_temodar.csv", row.names = NULL)
-sum(is.na(temodar$Start.Date)) #check
-temodar$Start_Year <- str_sub(temodar$Start.Date, -4, -1)
+# trial <- read.csv("~/repos/Bench2Bioinfo/Public-Repositories-Exploration/ClinTrialStats_temodar.csv", row.names = NULL)
+# trial <- read.csv("~/repos/Bench2Bioinfo/Public-Repositories-Exploration/ClinTrialStats_cisplatin.csv", row.names=NULL)
+trial <- read.csv("~/repos/Bench2Bioinfo/Public-Repositories-Exploration/ClinTrialStats_cyclophosphamide.csv", row.names=NULL)
+
+sum(is.na(trial$Start.Date)) #check
+trial$Start_Year <- str_sub(trial$Start.Date, -4, -1)
+
+## Simple baplot:
 barplot(
-  table(temodar$Start_Year), las=2, border=F,
+  table(trial$Start_Year), las=2, border=F,
   xlab = "Year",
   ylab = "Number of trials",
-  main = "Number of Trials with keyword 'Temodar'"
+  main = "Number of Trials with keyword"
 )
-## Year
-year.mat <- as.data.frame(table(temodar$Start_Year), stringsAsFactors=F)
-p1 <- ggplot(year.mat, aes(x=Var1, y=Freq)) + 
+
+## ggplot2: Year
+year.mat <- as.data.frame(table(trial$Start_Year), stringsAsFactors=F)
+p1 <- ggplot(year.mat, aes(x=factor(Var1), y=Freq)) + 
   geom_bar(stat="identity") +
   theme_bw() + 
-  labs(title="Number of trials with keyword 'Temodar'", x="") + 
-  scale_y_continuous(expand=c(0,0), limits=c(0,80)) +
+  labs(title="Number of trials with keyword 'cyclophosphamide'", x="") + 
+  scale_y_continuous(expand=c(0,0), limits=c(0,250)) +
   theme(axis.text.x=element_text(size=10, angle=45, hjust=1, face="bold"), axis.title=element_text(size=10))
 
 ## Phases
-phase.mat <- as.data.frame(table(temodar$Phases), stringsAsFactors=F)
+phase.mat <- as.data.frame(table(trial$Phases), stringsAsFactors=F)
 phase.mat$Var1[phase.mat$Var1==""] <- "(unlabeled)"
-p2 <- ggplot(phase.mat, aes(x=Var1, y=Freq)) + 
+p2 <- ggplot(phase.mat, aes(x=factor(Var1), y=Freq)) + 
   geom_bar(stat="identity") +
-  labs(x="", title="Distribution of type of trials since 1997") + 
-  scale_y_continuous(expand=c(0,0), limits=c(0,400)) +
+  labs(x="", title="Distribution of type of trials since 1976, cyclophosphamide") + 
+  scale_y_continuous(expand=c(0,0), limits=c(0,2000)) +
   theme_bw() + 
   theme(axis.text.x=element_text(size=10, angle=45, hjust=1, face="bold"), axis.title=element_text(size=10))
 
